384101a3e6040578ae7443c4822225512328542f
Tim Haak <tim@haak.co.uk>
2015-05-26 18:33:43 +0200

    removed some old code that isnt needed anymore

--- start.sh
+++ start.sh
@@ -8,20 +8,6 @@ touch /supervisord.log
 touch /supervisord.pid
 chown plex: /supervisord.log /supervisord.pid
 
-rm -rf /var/run/*
-rm -f "/config/Library/Application Support/Plex Media Server/plexmediaserver.pid"
-
-mkdir -p /var/run/dbus
-chown messagebus:messagebus /var/run/dbus
-dbus-uuidgen --ensure
-dbus-daemon --system --fork
-sleep 0.2
-
-avahi-daemon -D
-sleep 0.2
-
-# Get the proper group membership, credit to http://stackoverflow.com/a/28596874/249107
-
 TARGET_GID=$(stat -c "%g" /data)
 EXISTS=$(cat /etc/group | grep ${TARGET_GID} | wc -l)
 
